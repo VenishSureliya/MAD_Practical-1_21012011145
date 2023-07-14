32361487abf61d6a11e674cd81998ac928ac8477
student <student@gnu.ac.in>
2023-07-14 08:46:13 +0530

    Created Practical 1.2

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,24 @@
+fun main(){
+//    print("Hello World");
+
+    val intOne = 20;
+    val floatOne = 1.5;
+    val charOne = 'V';
+    val stringOne = "Venish";
+    val boolOne = true;
+    val doubleOne = 56.81;
+    val longOne = 3135685124;
+    val shortOne =  -5;
+    val byteOne = 127;
+
+
+    println("-------------------");
+    println("Int Value: " + intOne);
+    println("Float Value: " + floatOne);
+    println("Character Value: " + charOne);
+    println("String Value: " + stringOne);
+    println("Boolean Value: " + boolOne);
+    println("Short Value: " + shortOne);
+    println("Long Value: " + longOne);
+
+}
---
+++ src/main/kotlin/Practical-1.2.kt
@@ -0,0 +1,13 @@
+fun Main(){
+    val intOne = 10;
+    val intToDouble : Double = intOne.toDouble();
+    val stringOne = "10";
+    val stringToInt : Int = stringOne.toInt();
+    val stringTwo = "20";
+    val secondStringToInt : Int = stringTwo.toInt();
+    val stringThree = "11.65";
+    val stringToDouble : Double = stringThree.toDouble();
+
+
+
+}
